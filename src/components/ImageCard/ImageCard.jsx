92437a50554728d1cc63c3d34bb366216ed13351
olexiy-kireyko <a.kireyko@gmail.com>
2024-12-27 21:03:45 +0200

    fix img click

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,12 +1,17 @@
 import s from './ImageCard.module.css';
 
-const ImageCard = ({ image }) => {
+const ImageCard = ({ image, openModal, defineImageModalID }) => {
+  const handleClick = id => {
+    defineImageModalID(id);
+    openModal();
+  };
   return (
     <div className={s.image_card}>
       <img
         className={s.image_card_img}
         src={image.urls.small}
         alt={image.description}
+        onClick={() => handleClick(image.id)}
       />
     </div>
   );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -5,20 +5,16 @@ import { forwardRef } from 'react';
 const ImageGallery = forwardRef(function ImageGallery(props, ref) {
   const { images, openModal, defineImageModalID } = props;
 
-  const handleClick = id => {
-    defineImageModalID(id);
-    openModal();
-  };
   return (
     <ul ref={ref} className={s.image_gallery}>
       {images.map(image => {
         return (
-          <li
-            className={s.image_gallery_item}
-            key={image.id}
-            onClick={() => handleClick(image.id)}
-          >
-            <ImageCard image={image} />
+          <li className={s.image_gallery_item} key={image.id}>
+            <ImageCard
+              image={image}
+              openModal={openModal}
+              defineImageModalID={defineImageModalID}
+            />
           </li>
         );
       })}
